2cad1b3afd4a44d8264c84cf36afa8651438691b
Matyas Huba <hubamatyas@gmail.com>
2024-02-21 03:09:43 +0000

    Q3d

--- Coursework_01/Code/q3_d.py
+++ Coursework_01/Code/q3_d.py
@@ -14,40 +14,43 @@ from p2.low_level_environment import LowLevelEnvironment
 from p2.low_level_policy_drawer import LowLevelPolicyDrawer
 
 if __name__ == '__main__':
-    
-    # Get the map for the scenario
-    airport_map, drawer_height = full_scenario()
-    
-    # Set up the environment for the robot driving around
-    airport_environment = LowLevelEnvironment(airport_map)
-    
-    # Q3d:
-    # Configure the process model using different probabilities
-    airport_environment.set_nominal_direction_probability(.3)
+    p_values = [0.3, 0.6, 0.9, 1]
 
-    # Note that you can create multiple instances of the same object, with different
-    # settings, and run them in the same programme. Therefore, you do not need to
-    # create lots of separate scripts to run the code.
-
-    # Create the policy iterator
-    policy_solver = PolicyIterator(airport_environment)
-
-    # Set up initial state
-    policy_solver.initialize()
+    for p in p_values:
+        # Get the map for the scenario
+        airport_map, drawer_height = full_scenario()
         
-    # Bind the drawer with the solver
-    policy_drawer = LowLevelPolicyDrawer(policy_solver.policy(), drawer_height)
-    policy_solver.set_policy_drawer(policy_drawer)
-    
-    value_function_drawer = ValueFunctionDrawer(policy_solver.value_function(), drawer_height)
-    policy_solver.set_value_function_drawer(value_function_drawer)
+        # Set up the environment for the robot driving around
+        airport_environment = LowLevelEnvironment(airport_map)
         
-    # Compute the solution
-    v, pi = policy_solver.solve_policy()
-    
-    # Save screen shot; this is in the current directory
-    policy_drawer.save_screenshot("policy_iteration_results.pdf")
-    value_function_drawer.save_screenshot("value_function_results.pdf")
-    
-    # Wait for a key press
-    value_function_drawer.wait_for_key_press()
+        # Q3d:
+        # Configure the process model using different probabilities
+        airport_environment.set_nominal_direction_probability(p)
+
+        # Note that you can create multiple instances of the same object, with different
+        # settings, and run them in the same programme. Therefore, you do not need to
+        # create lots of separate scripts to run the code.
+
+        # Create the policy iterator
+        policy_solver = PolicyIterator(airport_environment)
+
+        # Set up initial state
+        policy_solver.initialize()
+            
+        # Bind the drawer with the solver
+        policy_drawer = LowLevelPolicyDrawer(policy_solver.policy(), drawer_height)
+        policy_solver.set_policy_drawer(policy_drawer)
+        
+        value_function_drawer = ValueFunctionDrawer(policy_solver.value_function(), drawer_height)
+        policy_solver.set_value_function_drawer(value_function_drawer)
+            
+        # Compute the solution
+        v, pi = policy_solver.solve_policy()
+        
+        # Save screen shot; this is in the current directory
+        p_str = str(p).replace(".", "_")
+        policy_drawer.save_screenshot(f"policy_p_{p_str}.pdf")
+        value_function_drawer.save_screenshot(f"value_p_{p_str}.pdf")
+        
+        # Wait for a key press
+        value_function_drawer.wait_for_key_press()
